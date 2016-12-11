7f743d34e2d46067503d678b9017896606ca70ba
James Martin <jamesmartin@github.com>
2016-12-10 22:41:16 -0800

    Style as as scrollable list

--- index.ios.js
+++ index.ios.js
@@ -8,29 +8,70 @@ import {
   AppRegistry,
   Button,
   ListView,
+  ScrollView,
   StyleSheet,
   Text,
+  TouchableWithoutFeedback,
   View
 } from 'react-native';
 
 const Environment = require('./environment.js')
 
+const styles = StyleSheet.create({
+  row: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+    //backgroundColor: '#3a5795',
+    margin: 5,
+  },
+  text: {
+    alignSelf: 'center',
+    color: 'grey',
+  },
+  scrollview: {
+    flex: 1,
+  }
+});
+
+class Row extends React.Component {
+  _onClick = () => {
+    this.props.onClick(this.props.data);
+  };
+
+  render() {
+    return (
+     <TouchableWithoutFeedback onPress={this._onClick} >
+        <View style={styles.row}>
+          <Text style={styles.text}>
+            {this.props.data}
+          </Text>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
 export default class Sf2Resistance extends Component {
   constructor(props) {
     super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      loadingText: "Loading...",
-      dataSource: ds.cloneWithRows([])
+      isRefreshing: false,
+      loaded: 0,
+      rowData: [],
     };
   }
 
+  _onClick = (row) => {
+    console.log('Clicked: ' + row)
+  }
+
   componentDidMount() {
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.fetchEvents().then((events) => {
       this.setState({
         loadingText: "",
-        dataSource: ds.cloneWithRows(events)
+        rowData: events,
       })
     })
   }
@@ -41,7 +82,6 @@ export default class Sf2Resistance extends Component {
       then((json) => {
         console.log("Loaded events")
         const events = (json || []).reduce((all, event) => {
-          console.log(event.group.category)
           if (event.group.category.shortname === "Social") {
             all.push(event.group.name)
           }
@@ -55,56 +95,16 @@ export default class Sf2Resistance extends Component {
       })
   }
 
-  reloadFeed() {
-    console.log("Refreshing feed");
-    this.fetchEvents().then((events) => {
-      this.setState({
-        loadingText: "",
-        dataSource: this.state.dataSource.cloneWithRows(events)
-      })
-    })
-  }
-
   render() {
+    const rows = this.state.rowData.map((row, ii) => {
+      return <Row key={ii} data={row} onClick={this._onClick} />
+    })
     return (
-      <View style={styles.container}>
-        <Text>{this.state.loadingText}</Text>
-        <ListView
-          enableEmptySections={true} //https://github.com/FaridSafi/react-native-gifted-listview/issues/39
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData}</Text>}
-        />
-
-        <Button
-          onPress={this.reloadFeed.bind(this)}
-          title="Refresh"
-          color="#65B9D5"
-          accessibilityLabel="Tap to refresh the feed"
-        />
-
-      </View>
+      <ScrollView style={styles.scrollView}>
+        {rows}
+      </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 22,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('Sf2Resistance', () => Sf2Resistance);
